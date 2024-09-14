080b310835095225ad1f067f130faa55b6ab89d0
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-14 15:31:32 +0600

    Up

--- HashMap01.java
+++ HashMap01.java
@@ -4,13 +4,13 @@ public class HashMap01 {
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
         // Put
-        System.out.println("===============");
+        System.out.println("======================");
         map.put("A", 01);
         System.out.println(map);
-        System.out.println("===============");
+        System.out.println("=====");
         map.put("B", 02);
         System.out.println(map);
-        System.out.println("===============");
+        System.out.println("==========");
         map.put("C", 03);
         System.out.println(map);
         System.out.println("===============");
@@ -19,33 +19,33 @@ public class HashMap01 {
         System.out.println("===============");
         // Get
         System.out.println(map.get("A"));
-        System.out.println("===============");
+        System.out.println("==");
         // Contains
         System.out.println(map.containsKey("A"));
-        System.out.println("===============");
+        System.out.println("=====");
         System.out.println(map.containsKey("a"));
-        System.out.println("===============");
+        System.out.println("=====");
         System.out.println(map.containsValue(0));
-        System.out.println("===============");
+        System.out.println("=====");
         System.out.println(map.containsValue(1));
-        System.out.println("===============");
+        System.out.println("=====");
         // Remove
         map.remove("B");
         System.out.println(map);
-        System.out.println("===============");
+        System.out.println("==========");
         map.put("B", 2);
         System.out.println(map);
         System.out.println("===============");
         // Print If Absent
         map.putIfAbsent("X", 100);
         System.out.println(map);
-        System.out.println("===============");
+        System.out.println("======================");
         map.putIfAbsent("A", 99);
         System.out.println(map);
-        System.out.println("===============");
+        System.out.println("======================");
         // Replace the key/value
         map.replace("X", 111);
         System.out.println(map);
-        System.out.println("===============");
+        System.out.println("======================");
     }
 }
