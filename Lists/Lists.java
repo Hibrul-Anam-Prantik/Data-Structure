606cfc7111f8d7b5114b2d13a9d9679cc7d79753
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-30 23:46:03 +0600

    ArrayList

---
+++ Lists/Lists.java
@@ -0,0 +1,11 @@
+package Lists;
+import java.util.ArrayList;
+import java.util.List;
+public class Lists
+{
+    public static void main(String[] args) {
+        List <String> li = new ArrayList<>();
+        li.add("DX");
+        System.out.println(li);
+    }
+}
